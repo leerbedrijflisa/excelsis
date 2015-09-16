261aceb4cb6e580663157f19aa6cc0c2666cca07
mattijn kleijer <mattijnkleijer@gmail.com>
2015-09-16 11:17:46 +0200

    Date will be initially filled with current date and can be changed afterwards

--- Lisa.Excelsis.WebClient/wwwroot/form.js
+++ Lisa.Excelsis.WebClient/wwwroot/form.js
@@ -3,12 +3,16 @@
         this.heading = "Form";
         this.firstName = "John";
         this.lastName = "Doe";
+
         this.currentDate = new Date();
-        //this.day = currentDate.getDay;
-        //this.month = currentDate.getMonth;
-        //this.year = currentDate.getYear;
-        //this.hours= currentDate.getHours;
-        //this.minutes = currentDate.getMinutes;
+        this.dd = this.currentDate.getDate();
+        this.MM = this.currentDate.getMonth() + 1;
+        this.yyyy = this.currentDate.getFullYear();
+        this.newDate = this.dd + "-" + this.MM + "-" + this.yyyy;
+
+        this.hh = this.currentDate.getHours();
+        this.mm = this.currentDate.getMinutes();
+        this.newTime = this.hh + ":" + this.mm;
     }
 
     get fullName(){
