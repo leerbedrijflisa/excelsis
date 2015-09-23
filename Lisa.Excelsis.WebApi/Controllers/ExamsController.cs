d3dad33be485b51daf67797580a2b3febbc687da
arthurvanstrien <arthurvanstrien@gmail.com>
2015-09-23 11:54:01 +0200

    worked on a Get request that gets selective data from the database. Note this does not work yet because the database is not updated yet.

--- Lisa.Excelsis.WebApi/Controllers/ExamsController.cs
+++ Lisa.Excelsis.WebApi/Controllers/ExamsController.cs
@@ -1,6 +1,7 @@
 ﻿using Lisa.Excelsis.WebApi.Models;
 using Microsoft.AspNet.Cors.Core;
 using Microsoft.AspNet.Mvc;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Lisa.Excelsis.WebApi
@@ -23,6 +24,41 @@ namespace Lisa.Excelsis.WebApi
                          select exams);
 
             return Json(query);
+        }
+
+        [HttpGet("{subjectId}/cohort/{cohort}")]
+        public object Get(int? subjectId = null, string cohort = null)
+        {
+            string method = "GET";
+            string request = "/subject/" + subjectId + "/cohort/" + cohort;
+
+            List<string> errors = new List<string>();
+
+            var query = (from exams in _db.Exams
+                         where exams.Subject == subjectId &&
+                         exams.Cohort == cohort
+                         select assessments.Criteria).SingleOrDefault();
+
+            if (query == null)
+            {
+                errors.Add("No exams where found");
+                return HttpError(request, method, 404, errors);
+            }
+
+            return Json(query);
+        }
+
+        // This creates a HTTP error code with json data
+        public object HttpError(string request, string method, int HttpStatusCode, List<string> message)
+        {
+            Response.StatusCode = HttpStatusCode;
+            var error = new Error();
+            error.HttpResponseCode = HttpStatusCode;
+            error.Request = request;
+            error.Method = method;
+            error.message = message;
+
+            return Json(error);
         }
     }
 }
