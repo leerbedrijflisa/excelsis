2708fecc4cb75b0789945b9f36c3799cd1cff702
William Willing <william@williamwilling.com>
2015-09-23 14:14:49 +0200

    Removed superfluous code.

--- Lisa.Excelsis.WebClient/wwwroot/app.js
+++ Lisa.Excelsis.WebClient/wwwroot/app.js
@@ -1,27 +1,12 @@
 ﻿import {inject} from 'aurelia-framework';
-//import {Router} from 'aurelia-router';
 import {HttpClient} from 'aurelia-http-client';
 
-//@inject(Router, HttpClient)
-export class App{
-
-    //private http: HttpClient = null;
-
-    configureRouter(config, router){
+export class App {
+    configureRouter(config, router) {
         config.title = "Excelsis";
         config.map([
-            { route: 'assessment', name: 'assessment', moduleId: 'assessment', nav: true },
-            { route: ['', 'exam'], name: 'exam', moduleId: 'exam', nav: true }
+            { route: 'assessment', name: 'assessment', moduleId: 'assessment' },
+            { route: ['', 'exam'], name: 'exam', moduleId: 'exam' }
         ]);
-
-        this.router = router;
-    }
-
-    constructor(){
-
-        this.http = new HttpClient().configure(x => {
-            x.withBaseUrl('http://localhost:5858/api');      
-            x.withHeader('accept', 'application/json')
-        });
     }
 }
