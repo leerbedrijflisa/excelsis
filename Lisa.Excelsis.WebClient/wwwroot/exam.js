1e84581f3999ea4335da0cd61e6deb2004c79241
William Willing <william@williamwilling.com>
2015-09-23 14:39:10 +0200

    Cleaned up exam view model and view.

--- Lisa.Excelsis.WebClient/wwwroot/exam.js
+++ Lisa.Excelsis.WebClient/wwwroot/exam.js
@@ -1,39 +1,20 @@
-﻿import {inject} from 'aurelia-framework';
-import {HttpClient} from 'aurelia-http-client';
+﻿import {HttpClient} from 'aurelia-http-client';
 
 export class Exam
 {
-    constructor() {
+    activate() {
         this.heading = "Exam";
         this.http = new HttpClient().configure(x => {
             x.withBaseUrl('http://localhost:5858');      
             x.withHeader('Content-Type', 'application/json')});
 
-        this.subjects = [ "Nederlands", "Applicatieontwikkelaar", "Rekenen" ];
+        this.subjects = [ 1, "Nederlands", "Applicatieontwikkelaar", "Rekenen" ];
         this.cohorts = [ "2015", "2014", "2013", "2012" ];
     }
 
     showExams() {
-        
-        var selectSubject = document.getElementById("selectExam");
-        var subject = selectSubject.options[selectSubject.selectedIndex].value;
-
-        var selectCohort = document.getElementById("selectCohort");
-        var cohort = selectCohort.options[selectCohort.selectedIndex].value;     
-        
         this.http.get("/exams").then(response => {
-            this.exams = this.findInObject(response.content, {subject: subject, cohort: cohort});
-              console.log(response.content);
+            this.exams = response.content;
         });
-    }
-
-    findInObject(myObject, myCriteria){
-
-        return myObject.filter(function(obj) {
-            return Object.keys(myCriteria).every(function(c) {
-                return obj[c] == myCriteria[c];
-            });
-        });
-
     }
 }
