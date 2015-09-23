b9d6240b88d5fdaa8f48ee310e4882a2d38e4106
Jim Geersinga <j.geersinga@outlook.com>
2015-09-23 13:52:29 +0200

    Created Questions Class for dbcontext Created Subjects Class for dbcontext

---
+++ Lisa.Excelsis.WebApi/Models/Subject.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lisa.Excelsis.WebApi.Models
+{
+    public class Subject
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
