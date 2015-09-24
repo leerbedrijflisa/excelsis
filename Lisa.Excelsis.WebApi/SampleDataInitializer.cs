3ef544999c322cf3867481ad9333b5765b6fb978
Jim Geersinga <j.geersinga@outlook.com>
2015-09-24 14:03:21 +0200

    Added questions to the exams

--- Lisa.Excelsis.WebApi/SampleDataInitializer.cs
+++ Lisa.Excelsis.WebApi/SampleDataInitializer.cs
@@ -1,4 +1,5 @@
 ﻿using Lisa.Excelsis.WebApi.Models;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Lisa.Excelsis.WebApi
@@ -18,21 +19,36 @@ namespace Lisa.Excelsis.WebApi
             var english = new Subject { Name = "Engels" };
             var mathematics = new Subject { Name = "Rekenen" };
 
+           
             if (!_db.Subjects.Any())
             {
                 _db.Subjects.AddRange(new Subject[] { dutch, softwareDevelopment, english, mathematics });
                 _db.SaveChanges();
 
-                _db.Exams.AddRange(new Exam[] {
-                    new Exam { Subject = dutch, Name = "Spreken", Cohort = "2015", Organization = "Da Vinci College" },
-                    new Exam { Subject = dutch, Name = "Schrijven", Cohort = "2015", Organization = "Da Vinci College" },
-                    new Exam { Subject = english, Name = "Spreken", Cohort = "2015", Organization = "Da Vinci College" },
-                    new Exam { Subject = softwareDevelopment, Name = "Ontwerpen van een applicatie", Cohort = "2015", Organization = "Da Vinci College" },
-                    new Exam { Subject = softwareDevelopment, Name = "Realiseren van een applicatie", Cohort = "2015", Organization = "Da Vinci College" },
-                    new Exam { Subject = softwareDevelopment, Name = "Opleveren van een applicatie", Cohort = "2015", Organization = "Da Vinci College" },
-                    new Exam { Subject = mathematics, Name = "Verhoudingen", Cohort = "2015", Organization = "Da Vinci College" },
-                    new Exam { Subject = mathematics, Name = "Meten en meetkunde", Cohort = "2015", Organization = "Da Vinci College" }
+                _db.Exams.AddRange(new Exam[] { 
+                    new Exam { Subject = dutch, Name = "Spreken", Cohort = "2015", Organization = "Da Vinci College"},
+                    new Exam { Subject = dutch, Name = "Schrijven", Cohort = "2015", Organization = "Da Vinci College"},
+                    new Exam { Subject = english, Name = "Spreken", Cohort = "2015", Organization = "Da Vinci College"},
+                    new Exam { Subject = softwareDevelopment, Name = "Ontwerpen van een applicatie", Cohort = "2015", Organization = "Da Vinci College"},
+                    new Exam { Subject = softwareDevelopment, Name = "Realiseren van een applicatie", Cohort = "2015", Organization = "Da Vinci College"},
+                    new Exam { Subject = softwareDevelopment, Name = "Opleveren van een applicatie", Cohort = "2015", Organization = "Da Vinci College"},
+                    new Exam { Subject = mathematics, Name = "Verhoudingen", Cohort = "2015", Organization = "Da Vinci College"},
+                    new Exam { Subject = mathematics, Name = "Meten en meetkunde", Cohort = "2015", Organization = "Da Vinci College"}
                 });
+                _db.SaveChanges();
+                
+                foreach (var exam in _db.Exams)
+                {                   
+                    for(var i = 0; i < 10; i++)
+                    {
+                        _db.Questions.Add(new Question
+                        {
+                            Description = "Hier hoord vraag " + i + " te staan.",
+                            Rating = 0,
+                            ExamId = exam.Id
+                        });
+                    }  
+                }
                 _db.SaveChanges();
             }
         }
