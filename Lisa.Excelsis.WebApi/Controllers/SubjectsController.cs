048404a633fd078ac8f127006e131736ad434453
Jim Geersinga <j.geersinga@outlook.com>
2015-09-28 14:34:51 +0200

    Add subject controller

---
+++ Lisa.Excelsis.WebApi/Controllers/SubjectsController.cs
@@ -0,0 +1,39 @@
+﻿using Lisa.Excelsis.WebApi.Models;
+using Microsoft.AspNet.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lisa.Excelsis.WebApi.Controllers
+{
+    public class SubjectsController : Controller
+    {
+        private readonly ExcelsisDb _db;
+        public SubjectsController(ExcelsisDb db)
+        {
+            _db = db;
+        }
+
+        // GET: subjects
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var query = (from subjects in _db.Subjects
+                         select subjects);
+
+            return new ObjectResult(query);
+        }
+
+        // GET subjects/{subject}
+        [HttpGet("{name}")]
+        public IActionResult Get(string name)
+        {
+            var query = (from subjects in _db.Subjects
+                         where subjects.Name.ToLower() == name.ToLower()
+                         select subjects);
+
+            return new ObjectResult(query);
+        }
+    }
+}
