d0cc342707a12e94def5dfe8d54c455f6f64bf6b
Jim Geersinga <j.geersinga@outlook.com>
2015-09-30 09:22:00 +0200

    Moved criterium out of the assessment class

---
+++ Lisa.Excelsis.WebApi/Models/Criterium.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lisa.Excelsis.WebApi.Models
+{
+    public class Criterium
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Question { get; set; }
+        public int AssessmentId { get; set; }
+        public int Rating { get; set; }
+        public bool? Answer { get; set; }
+        public bool[] CriteriumBoxes { get; set; }
+    }
+}
