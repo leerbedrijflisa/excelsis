a69082338aa662dd81139090249c34ee8a871e83
Jim Geersinga <j.geersinga@outlook.com>
2015-10-02 11:24:53 +0200

    Created a database connect function for the webapi to connect to the classlibrary

---
+++ Lisa.Excelsis.Data/Database.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Lisa.Excelsis.Data
+{
+    public partial class Database
+    {
+        private static readonly ExcelsisDb _db = new ExcelsisDb();
+    }
+}
