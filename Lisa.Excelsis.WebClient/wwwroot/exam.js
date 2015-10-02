06d7bbde28df53ac16f0582e1985d70570e0374e
mattijn kleijer <mattijnkleijer@gmail.com>
2015-10-02 11:40:15 +0200

    teacher can now select which exam he wants to select

--- Lisa.Excelsis.WebClient/wwwroot/exam.js
+++ Lisa.Excelsis.WebClient/wwwroot/exam.js
@@ -8,13 +8,17 @@ export class Exam
             x.withBaseUrl('http://localhost:5858');      
             x.withHeader('Content-Type', 'application/json')});
 
-        this.subjects = [ 1, "Nederlands", "Applicatieontwikkelaar", "Rekenen" ];
+        this.subjects = [ "Nederlands", "Applicatieontwikkelaar", "Rekenen" ];
         this.cohorts = [ "2015", "2014", "2013", "2012" ];
     }
 
     showExams() {
-        this.http.get("/exams").then(response => {
+       
+        var subject = document.getElementById('subject').value;
+        var cohort = document.getElementById('cohort').value;
+        this.http.get("/exams/"+subject+"/"+cohort).then(response => {
             this.exams = response.content;
+            
         });
     }
 }
