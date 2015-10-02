330d57cc9a934eeed52508a28b3f8395a510d3a7
arthurvanstrien <arthurvanstrien@gmail.com>
2015-10-02 10:14:29 +0200

    Reformatted the dateTime that was send to the web api.

--- Lisa.Excelsis.WebClient/wwwroot/assessment.js
+++ Lisa.Excelsis.WebClient/wwwroot/assessment.js
@@ -41,10 +41,10 @@ export class Welcome{
                 "name": this.name,
                 "number": this.number
             },
-            "assessor": [{
+            "assessors": [{
                 "userName": "joostronkesagerbeek"
             }],
-            "assessed": this.newDate+"T"+this.newTime+":00Z"
+            "assessed": formatDate(this.newDate, this.newTime)
         };
 
         this.http.post("assessments/"+this.subject+"/"+this.examName+"/"+this.cohort, Content).then(response => {
@@ -52,5 +52,33 @@ export class Welcome{
             this.assessment = response.content;
             alert(this.assessment);
         });
+
+        function formatDate(date, time){
+
+            var splitDate = date.split("-");
+            var splitTime = time.split(":");
+
+            //If the month is below 10 it adds a 0 up front.
+            if(splitDate[1] < 10){
+                splitDate[1] = "0"+splitDate[1];
+            }
+
+            //If the day is below 10 it adds a 0 up front.
+            if(splitDate[0] < 10){
+                splitDate[0] = "0"+splitDate[0];
+            }
+
+            //If the hours are below 10 it adds a 0 up front.
+            if(splitTime[0] < 10){
+                splitTime[0] = "0"+splitTime[0];
+            }
+
+            //If the minutes are below 10 it adds a 0 up front.
+            if(splitTime[1] < 10){
+                splitTime[1] = "0"+splitTime[1];
+            }
+
+            return splitDate[2]+"-"+splitDate[1]+"-"+splitDate[0]+"T"+splitTime[0]+":"+splitTime[1]+":00Z"
+        }
     }
 }
