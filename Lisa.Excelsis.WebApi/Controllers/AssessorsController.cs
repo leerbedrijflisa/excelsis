9413af1e416fa3c2b3ce81b8f9cc2971da28ca70
Jim Geersinga <j.geersinga@outlook.com>
2015-10-05 13:21:40 +0200

    Added a new assessorController

---
+++ Lisa.Excelsis.WebApi/Controllers/AssessorsController.cs
@@ -0,0 +1,30 @@
+﻿using Lisa.Excelsis.Data;
+using Microsoft.AspNet.Mvc;
+using System.Linq;
+
+namespace Lisa.Excelsis.WebApi.Controllers
+{
+    public class AssessorsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var query = (from assessor in _db.FetchAssessors()
+                         select assessor);
+
+            return new ObjectResult(query);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var query = (from assessor in _db.FetchAssessors()
+                         where assessor.Id == id
+                         select assessor);
+
+            return new ObjectResult(query);
+        }
+
+        private readonly Database _db = new Database();
+    }
+}
