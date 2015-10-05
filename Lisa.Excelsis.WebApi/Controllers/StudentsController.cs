e7b56d9eb9a0b41a178e3418d5ad0426cc070c04
Jim Geersinga <j.geersinga@outlook.com>
2015-10-05 13:18:18 +0200

    Added a new studentController

---
+++ Lisa.Excelsis.WebApi/Controllers/StudentsController.cs
@@ -0,0 +1,29 @@
+﻿using Lisa.Excelsis.Data;
+using Microsoft.AspNet.Mvc;
+using System.Linq;
+
+namespace Lisa.Excelsis.WebApi.Controllers
+{
+    public class StudentsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var query = (from student in _db.FetchStudents()
+                         select student);
+
+            return new ObjectResult(query);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var query = (from student in _db.FetchStudents()
+                         where student.Id == id
+                         select student);
+            return new ObjectResult(query);
+        }
+
+        private readonly Database _db = new Database();
+    }
+}
