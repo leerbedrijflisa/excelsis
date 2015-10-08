ecb0a0884b7b575118a41c3eab9f4076bd528761
Jim Geersinga <j.geersinga@outlook.com>
2015-10-08 13:49:24 +0200

    added navigation to the webclient

--- Lisa.Excelsis.WebClient/wwwroot/app.js
+++ Lisa.Excelsis.WebClient/wwwroot/app.js
@@ -1,11 +1,13 @@
 ﻿export class App {
     configureRouter(config, router) {
+        this.router = router;
+
         config.title = "Excelsis";
         config.map([
-            { route: 'assessment', name: 'assessment', moduleId: 'assessment' },
-            { route: 'assessment/:urlId', name: 'assessmentId', moduleId: 'assessment' },
-            { route: ['', 'exam'], name: 'exam', moduleId: 'exam' },
-            { route: 'version', name: 'version', moduleId: 'version'}
+            { route: ['', 'exam'], name: 'exam', moduleId: 'exam', nav: true, title:'Home'  },
+            { route: 'assessment', name: 'assessment', moduleId: 'assessment', nav: true, title:'Assessment' },
+            { route: 'assessment/:urlId', name: 'assessmentId', moduleId: 'assessment' },           
+            { route: 'version', name: 'version', moduleId: 'version', nav: true, title:'Version' }
         ]);
     }
 }
