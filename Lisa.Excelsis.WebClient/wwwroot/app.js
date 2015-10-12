31e9b7053c4cff73b29fad95b676534835d586fb
arthurvanstrien <arthurvanstrien@gmail.com>
2015-10-12 11:51:20 +0200

    Added a route to the new assessments page.

--- Lisa.Excelsis.WebClient/wwwroot/app.js
+++ Lisa.Excelsis.WebClient/wwwroot/app.js
@@ -6,7 +6,8 @@
         config.map([
             { route: ['', 'exam'], name: 'exam', moduleId: 'exam', nav: true, title:'Home'  },
             { route: 'assessment', name: 'assessment', moduleId: 'assessment', nav: true, title:'Assessment' },
-            { route: 'assessment/:urlId', name: 'assessmentId', moduleId: 'assessment' },           
+            { route: 'assessment/:urlId', name: 'assessmentId', moduleId: 'assessment' },
+            { route: 'assessments', name: 'assessments', moduleId: 'assessments', nav: true, title:'Assessments'}, 
             { route: 'version', name: 'version', moduleId: 'version', nav: true, title:'Version' }
         ]);
     }
