9fb75ff7a75c74d435e40dcb86c5dff6d78893a3
Jim Geersinga <j.geersinga@outlook.com>
2015-10-13 12:37:27 +0200

    Created a new file utils that contain certain functions

---
+++ Lisa.Excelsis.WebClient/wwwroot/Utils.js
@@ -0,0 +1,20 @@
+﻿export class Utils {
+
+    doubleDigit(digit){
+        if (digit.length < 2)
+        { 
+            digit = "0" + digit;
+        }
+        return digit;
+    }
+
+    formatDate(date, time){
+        var splitDate = date.split("-");
+        var splitTime = time.split(":");
+
+        return splitDate[2] + "-" + this.doubleDigit(splitDate[0]) + 
+                              "-" + this.doubleDigit(splitDate[1]) +
+                              "T" + this.doubleDigit(splitTime[0]) +
+                              ":" + this.doubleDigit(splitTime[1]) + ":00Z";
+    }
+}
