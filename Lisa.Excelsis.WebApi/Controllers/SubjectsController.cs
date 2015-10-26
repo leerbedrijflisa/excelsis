035a7ea3d0b4198ad229b5cca669a3e5e8693963
Jim Geersinga <j.geersinga@outlook.com>
2015-10-26 09:33:52 +0100

    removed unused usings

--- Lisa.Excelsis.WebApi/Controllers/SubjectsController.cs
+++ Lisa.Excelsis.WebApi/Controllers/SubjectsController.cs
@@ -1,11 +1,7 @@
 ﻿using Lisa.Excelsis.Data;
 using Lisa.Excelsis.WebApi.TransferObjects;
 using Microsoft.AspNet.Mvc;
-using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
-using System;
-using System.Text.RegularExpressions;
 
 namespace Lisa.Excelsis.WebApi.Controllers
 {
