0bf8e6b6fd3175e6543e447ed216356fa7f6d049
mattijn kleijer <mattijnkleijer@gmail.com>
2015-10-29 11:52:19 +0100

    created feature for specific student assessment page

--- Lisa.Excelsis.WebClient/wwwroot/assessment/list.js
+++ Lisa.Excelsis.WebClient/wwwroot/assessment/list.js
@@ -1,27 +1,47 @@
-﻿import {HttpClient} from 'aurelia-http-client';
+﻿import {Router} from 'aurelia-router';
+import {HttpClient} from 'aurelia-http-client';
 
 export class List
 {
     static inject() {
-        return [ HttpClient ];
+        return [ Router, HttpClient ];
     }
 
-    constructor(http){
+    constructor(router, http){
         this.http = http;
+        this.router = router;
     }
 
     activate() {
-        this.message = "Beoordelingen worden geladen. Een moment geduld astublieft...";
+        this.message = "Studenten worden geladen. Een moment geduld astublieft...";
 
         this.heading = "Assessments";
        
-        this.http.get("/assessments/").then(response => {
-            this.assessments = response.content;
+        this.http.get("/students/").then(response => {
+            this.students = response.content;
             this.message = null;
         }, response => {
             if(response.statusCode == 404){
-                this.message = "Helaas er zijn geen beoordelingen gevonden.";
+                this.message = "Helaas er zijn geen studenten gevonden.";
             }
         });
+    }
+    
+    showAssessments(student){
+        var Student = document.getElementById('student').value;
+        var url = (student == "student")?  "/assessments/student/"+Student : "/assessments";
+        this.http.get(url).then(response => {
+            this.assessments = response.content;
+            this.message = null;
+            document.getElementById("assessments").style.display = "inline";
+        }, response => {
+            if(response.statusCode == 404){
+                this.message = "Helaas er zijn geen assessments gevonden.";
+                document.getElementById("assessments").style.display = "none";
+            }
+        });
+    }
+    openAssessment(name, subject, cohort, id) {
+        this.router.navigateToRoute('assessmentId', { subject: subject, name: name,  cohort: cohort, assessmentid: id });
     }
 }
