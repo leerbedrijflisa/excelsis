e2411b7560b81d0b715187cbd4f5103db5acb52d
Jim Geersinga <j.geersinga@outlook.com>
2015-11-03 11:15:25 +0100

    commented out subjects

--- Lisa.Excelsis.WebApi/Controllers/SubjectsController.cs
+++ Lisa.Excelsis.WebApi/Controllers/SubjectsController.cs
@@ -1,29 +1,30 @@
-﻿using Lisa.Excelsis.Data;
-using Lisa.Excelsis.WebApi.TransferObjects;
+﻿using Lisa.Excelsis.WebApi.TransferObjects;
 using Microsoft.AspNet.Mvc;
-using System.Linq;
 
-namespace Lisa.Excelsis.WebApi.Controllers
+namespace Lisa.Excelsis.WebApi
 {
     [Route("[controller]")]
     public class SubjectsController : Controller
     {
-        [HttpGet]
-        public IActionResult Get([FromQuery] Filter filter)
-        {
-            var subjects = _db.FetchSubjects(filter.Assessor);
-            var result = SubjectMapper.ToTransferObjects(subjects);
-            return new HttpOkObjectResult(result);
-        }
+        //[HttpGet]
+        //public IActionResult Get([FromQuery] Filter filter)
+        //{
+        //    var results = _db.Select<SubjectInfo>(
+        //          "select * from Subjects");
 
-        // GET subjects/{subject}
-        [HttpGet("{name}")]
-        public IActionResult Get(string name)
-        {
-            var subject = _db.FetchSubjects().Where(s => s.Name.ToLower() == name.ToLower());
-            var result = SubjectMapper.ToTransferObject(subject);
-            return new HttpOkObjectResult(result);
-        }
+        //    return new HttpOkObjectResult(results);
+        //}
+
+        //// GET subjects/{subject}
+        //[HttpGet("{name}")]
+        //public IActionResult Get(string name)
+        //{
+        //    var result = _db.Select<Subject>(
+        //          "Select * from Subjects " +
+        //          "where Name = '" + name + "'");
+
+        //    return new HttpOkObjectResult(result);
+        //}
 
         private readonly Database _db = new Database();
     }
---
+++ Lisa.Excelsis.WebApi/Database/SubjectsModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Lisa.Excelsis.WebApi
+{
+    public class SubjectsInfo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
